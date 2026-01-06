69e5fda0da73c8c82e7b70c73bda64e44c73e1ee
Yu Shiyang <yu.shiyang@gnayihs.uy>
2026-01-07 01:28:50 +0800

    Store makernote in temp file instead of bash variable to preserve null chars
    A quick look suggests that the out-of-camera UserComment field, when interpreted as a binary stream, is always terminated by two null characters.

--- vivo-x300-1-maker-note.sh
+++ vivo-x300-1-maker-note.sh
@@ -14,25 +14,33 @@ while read -r file; do
 	input_file_base_name="$(basename "$input_file" .jpg)"
 	input_file_parent_dir="$(dirname "$input_file")"
 
+	# Manually create temp file path instead of using mktemp in order to support exiftool on Windows
+	maker_note_temp_file="$INPUT_DIR/${file%.jpg}.temp"
+	while [ -f "$maker_note_temp_file" ]; do
+		maker_note_temp_file="$maker_note_temp_file.temp"
+	done
+
 	# The EXIF specification requires Unicode text to be encoded in UTF-16 and be prepended with "UNICODE\0"
 	# The vivo X300 stock camera does neither of these, instead writing plain UTF-8 directly to the EXIF UserComment
 	# This UTF-8 can be read directly as binary (which also serves to preserve line breaks)
-	maker_note="$(exiftool -b -EXIF:UserComment "$input_file")"
+	exiftool -b -EXIF:UserComment "$input_file" > "$maker_note_temp_file"
 
-	if [ -n "$maker_note" ]; then
+	if [ -s "$maker_note_temp_file" ]; then
 
 		# Fix maker note of jpg
-		exiftool -EXIF:UserComment= -XMP-exif:MakerNote="$maker_note" -overwrite_original "$input_file"
+		exiftool -EXIF:UserComment= -XMP-exif:MakerNote'<='"$maker_note_temp_file" -overwrite_original "$input_file"
 
 		# Fix maker note of any XMP sidecar files
 		readarray -d '' XMP_FILES < <( find "$input_file_parent_dir" -type f -name "$input_file_base_name"'.[xX][mM][pP]' -print0 )
 		for xmp_file in "${XMP_FILES[@]}"; do
-			exiftool -EXIF:UserComment= -XMP-exif:MakerNote="$maker_note" -overwrite_original "$xmp_file"
+			exiftool -EXIF:UserComment= -XMP-exif:MakerNote'<='"$maker_note_temp_file" -overwrite_original "$xmp_file"
 		done
 
 	else
 		empty_maker_note_photos+=("$input_file")
 	fi
+
+	rm "$maker_note_temp_file"
 
 done <<< "$(find "$INPUT_DIR" -type f -name '*.jpg' -printf '%P\n')"
 
